bc8f54f045172474760bf1be3292555cada2cb99
Nathalie Vialaneix <nathalie.vialaneix@inrae.fr>
2022-02-20 13:20:02 +0000

    version 0.1-3

--- R/sparseSIR.R
+++ R/sparseSIR.R
@@ -42,8 +42,8 @@
 #' simulate_bm <- function() return(c(0, cumsum(rnorm(length(tsteps)-1, sd=1))))
 #' x <- t(replicate(nsim, simulate_bm()))
 #' beta <- cbind(sin(tsteps*3*pi/2), sin(tsteps*5*pi/2))
-#' beta[((tsteps < 0.2) || (tsteps > 0.5)), 1] <- 0
-#' beta[((tsteps < 0.6) || (tsteps > 0.75)), 2] <- 0
+#' beta[((tsteps < 0.2) | (tsteps > 0.5)), 1] <- 0
+#' beta[((tsteps < 0.6) | (tsteps > 0.75)), 2] <- 0
 #' y <- log(abs(x %*% beta[ ,1]) + 1) + sqrt(abs(x %*% beta[ ,2]))
 #' y <- y + rnorm(nsim, sd = 0.1)
 #' res_ridge <- ridgeSIR(x, y, H = 10, d = 2, mu2 = 10^8)
@@ -253,8 +253,8 @@ print.sparseRes <- function(x, ...) {
 #' simulate_bm <- function() return(c(0, cumsum(rnorm(length(tsteps)-1, sd=1))))
 #' x <- t(replicate(nsim, simulate_bm()))
 #' beta <- cbind(sin(tsteps*3*pi/2), sin(tsteps*5*pi/2))
-#' beta[((tsteps < 0.2) || (tsteps > 0.5)), 1] <- 0
-#' beta[((tsteps < 0.6) || (tsteps > 0.75)), 2] <- 0
+#' beta[((tsteps < 0.2) | (tsteps > 0.5)), 1] <- 0
+#' beta[((tsteps < 0.6) | (tsteps > 0.75)), 2] <- 0
 #' y <- log(abs(x %*% beta[ ,1]) + 1) + sqrt(abs(x %*% beta[ ,2]))
 #' y <- y + rnorm(nsim, sd = 0.1)
 #' \dontrun{
@@ -327,8 +327,8 @@ project <- function(object) {
 #' simulate_bm <- function() return(c(0, cumsum(rnorm(length(tsteps)-1, sd=1))))
 #' x <- t(replicate(nsim, simulate_bm()))
 #' beta <- cbind(sin(tsteps*3*pi/2), sin(tsteps*5*pi/2))
-#' beta[((tsteps < 0.2) || (tsteps > 0.5)), 1] <- 0
-#' beta[((tsteps < 0.6) || (tsteps > 0.75)), 2] <- 0
+#' beta[((tsteps < 0.2) | (tsteps > 0.5)), 1] <- 0
+#' beta[((tsteps < 0.6) | (tsteps > 0.75)), 2] <- 0
 #' y <- log(abs(x %*% beta[ ,1]) + 1) + sqrt(abs(x %*% beta[ ,2]))
 #' y <- y + rnorm(nsim, sd = 0.1)
 #' res_ridge <- ridgeSIR(x, y, H = 10, d = 2, mu2 = 10^8)
